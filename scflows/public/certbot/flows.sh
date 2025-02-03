79eb41a1fecb064e5c499b24239a22e89b3e57da
oscgonfer <oscgonfer@gmail.com>
2025-02-03 17:01:13 +0000

    Minor fixes to reload nginx after post-renewal hook

--- scflows/public/certbot/flows.sh
+++ scflows/public/certbot/flows.sh
@@ -8,4 +8,7 @@ cp -L -r /etc/letsencrypt/live/$DOMAIN/*.pem /root/smartcitizen-flows/scflows/pu
 cp /etc/letsencrypt/ssl-dhparams.pem /root/smartcitizen-flows/scflows/public/certbot/www/
 cp /etc/letsencrypt/options-ssl-nginx.conf /root/smartcitizen-flows/scflows/public/certbot/www/
 
+# Gracefully reload nginx after
+echo 'Reloading nginx...'
+docker exec -it smartcitizen-flows-proxy-1 nginx -s reload
 echo 'Done'
